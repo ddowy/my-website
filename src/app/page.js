267271eccf23fe11cd5ce0c263bfda9eeab707ef
ddowy <92402470+ddowy@users.noreply.github.com>
2023-07-11 21:04:51 -0500

    Basic homepage set up like header and footer placement and placement for image display

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,29 @@ import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <main>
-      <h2>YAB Website WOOHOO</h2>
+    <main className={styles.main}>
+      <div style={{
+        backgroundColor: 'maroon', 
+        width: '100%',    
+        height: '500px',
+        color: 'white',
+        fontSize: '2rem',
+        alignItems: 'center',
+        display: 'flex',
+        justifyContent: 'center'
+      }}>
+        Image Display Box</div>
+      <div style={{
+        backgroundColor: 'navy',
+        width: '100%',    
+        height: '500px',
+        color: 'white',
+        fontSize: '2rem',
+        alignItems: 'center',
+        display: 'flex',
+        justifyContent: 'center'
+      }}>
+      Rest of homepage</div>
     </main>
   )
 }
